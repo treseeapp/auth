8804f98bd99382d5ad262ce33c1c630d9921b4c9
joanpacheco <joanantonipacheco@gmail.com>
2020-03-22 14:03:12 +0100

    Añadidos los endpoints para el Inicio de Sesión con Google.
    /auth/google
    /aut/google/callback

--- src/controller/loginController.ts
+++ src/controller/loginController.ts
@@ -13,4 +13,27 @@ require('../config/Passport');
 export class LoginController {
 
 
+    @Get('google')
+    @Middleware(passport.authenticate('google', {scope: ['profile', 'email']}))
+    private loginGoogle(req: Request, res: Response) {
+        res.end();
+    }
+
+    @Get('google/callback')
+    @Middleware(passport.authenticate('google'))
+    private loginGoogleCallBack(req: Request, res: Response) {
+
+        // Demomento solo guardamos el correo en el token
+        const accessToken = jwt.sign({user: req.user}, <string>process.env.TOKEN_SECRET_KEY, {
+            expiresIn: process.env.ACCES_TOKEN_EXPIRE,
+        });
+
+        const refreshToken = jwt.sign({user: req.user}, <string>process.env.TOKEN_SECRET_KEY, {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRE,
+        });
+
+        // Habrá que modificar la ruta si es otra más adelante
+        res.redirect(301, process.env.FRONTEND_URL + '/#/admin/?accessToken=' + accessToken + "&refreshToken=" + refreshToken);
+    }
+
 }
